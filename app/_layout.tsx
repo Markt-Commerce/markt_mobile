4f3a779c273e13cc2f4d708a5a14b86d6b413b6b
Joshua <amethystguitaristj@gmail.com>
2025-07-23 02:54:19 +0100

    configured to use nativewind for styling

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import "@/global.css"
+
+const _layout = () => {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
+
+export default _layout
+
+const styles = StyleSheet.create({})
